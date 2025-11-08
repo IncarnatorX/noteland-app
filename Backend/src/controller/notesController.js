e1350aea0b27a2e41f3efe4fc912864787c0bfaf
vishnu <hikingfaab@gmail.com>
2025-11-08 15:17:04 +0530

    feat: implemented updating notes

--- Backend/src/controller/notesController.js
+++ Backend/src/controller/notesController.js
@@ -1,5 +1,34 @@
 import db from "../DB/db.js";
 
+const getAllNotes = async (req, res) => {
+  try {
+    const allNotes = await db.query(`
+      SELECT notes.id, notes.title, notes.content, COALESCE(json_agg(tasks) FILTER (WHERE tasks.id IS NOT NULL), '[]') AS tasks 
+      FROM notes 
+      LEFT JOIN tasks ON notes.id = tasks.note_id  
+      GROUP BY NOTES.id
+      `);
+
+    if (allNotes?.rows?.length === 0) {
+      return res
+        .status(204)
+        .json({ message: "No Notes available.", success: false });
+    }
+
+    return res.status(200).json({
+      message: "All notes fetched successfully.",
+      payload: allNotes.rows,
+      success: true,
+    });
+  } catch (error) {
+    console.error("Error in createNote controller: ", error.message);
+    return res.status(500).json({
+      message: "Something wen't wrong at our end. Please try again.",
+      success: false,
+    });
+  }
+};
+
 const getAllUserNotes = async (req, res) => {
   try {
     const userId = req.user.id;
@@ -77,7 +106,7 @@ const createNote = async (req, res) => {
 
     return res.status(200).json({
       message: "A new note created successfully.",
-      payload: result.rows[0],
+      data: result.rows[0],
       success: true,
     });
   } catch (error) {
@@ -85,6 +114,7 @@ const createNote = async (req, res) => {
     return res.status(500).json({
       message: "Something wen't wrong at our end. Please try again.",
       success: false,
+      data: null,
     });
   }
 };
@@ -138,6 +168,72 @@ const createTask = async (req, res) => {
   }
 };
 
+const updateNote = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    if (!userId) {
+      return res.status(403).json({
+        message: "Unauthorized request received. Please try again.",
+        success: false,
+      });
+    }
+
+    if (userId !== parseInt(req.body.user_id)) {
+      return res.status(403).json({
+        message: "Unauthorized. You're not allowed to update the note.",
+        success: false,
+      });
+    }
+
+    const noteId = req.body.id;
+
+    if (!noteId) {
+      return res.status(403).json({
+        message: "Invalid request to update the note",
+        success: false,
+      });
+    }
+
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+      return res.status(403).json({
+        message: "Invalid request to update the note",
+        success: false,
+      });
+    }
+
+    const result = await db.query(
+      `
+      UPDATE notes 
+      SET title = $1, content = $2 WHERE id = $3 RETURNING *
+      `,
+      [title, content, noteId]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        message: "Unable to update the note.",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Note successfully updated.",
+      success: true,
+      data: result.rows[0],
+    });
+  } catch (error) {
+    console.error("Error in updateNote controller: ", error.message);
+    return res.status(500).json({
+      message: "Something wen't wrong at our end. Please try again.",
+      data: null,
+      success: false,
+    });
+  }
+};
+
 const updateTask = async (req, res) => {
   try {
     const userId = req.user.id;
@@ -187,35 +283,6 @@ const updateTask = async (req, res) => {
   }
 };
 
-const getAllNotes = async (req, res) => {
-  try {
-    const allNotes = await db.query(`
-      SELECT notes.id, notes.title, notes.content, COALESCE(json_agg(tasks) FILTER (WHERE tasks.id IS NOT NULL), '[]') AS tasks 
-      FROM notes 
-      LEFT JOIN tasks ON notes.id = tasks.note_id  
-      GROUP BY NOTES.id
-      `);
-
-    if (allNotes?.rows?.length === 0) {
-      return res
-        .status(204)
-        .json({ message: "No Notes available.", success: false });
-    }
-
-    return res.status(200).json({
-      message: "All notes fetched successfully.",
-      payload: allNotes.rows,
-      success: true,
-    });
-  } catch (error) {
-    console.error("Error in createNote controller: ", error.message);
-    return res.status(500).json({
-      message: "Something wen't wrong at our end. Please try again.",
-      success: false,
-    });
-  }
-};
-
 const deleteNote = async (req, res) => {
   try {
     const userId = req.user.id;
@@ -256,12 +323,10 @@ const deleteNote = async (req, res) => {
       .json({ message: "Note Deleted successfully.", success: true });
   } catch (error) {
     console.error("Error in deleteNote controller: ", error.message);
-    return res
-      .status(500)
-      .json({
-        message: "Something wen't wrong at our end. Please try again.",
-        success: false,
-      });
+    return res.status(500).json({
+      message: "Something wen't wrong at our end. Please try again.",
+      success: false,
+    });
   }
 };
 
@@ -270,6 +335,7 @@ export {
   getAllNotes,
   getAllUserNotes,
   createTask,
+  updateNote,
   updateTask,
   deleteNote,
 };
